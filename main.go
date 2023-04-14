6bbf63791177624491b2c14069a585875fc0a25c
01vishu <01vishuverma@gmail.com>
2023-04-14 23:15:53 +0530

    Add booked user function

--- main.go
+++ main.go
@@ -2,28 +2,38 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	conferenceName := "Go Conference"
-	const conferenceTickets int=50
-	var remaningTickets uint=50
-	
-	fmt.Printf("Welcome to %v booking appliction\n",conferenceName)
-	fmt.Printf("We have total of %v tickets and %v are still available.\n",conferenceTickets,remaningTickets)
+	const conferenceTickets int = 50
+	var remaningTickets uint = 50
+
+	fmt.Printf("Welcome to %v booking appliction\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remaningTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	var booking []string
 
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets int
+	var userTickets uint
 	//ask user for their name
 	fmt.Print("Enter the user first name: ")
 	fmt.Scan(&firstName)
 	fmt.Print("Enter the user last name: ")
 	fmt.Scan(&lastName)
-	fmt.Print("Enter the user email: ")
+	fmt.Print("Enter the email address: ")
 	fmt.Scan(&email)
 	fmt.Print("How much ticket you want to book: ")
 	fmt.Scan(&userTickets)
 
-	fmt.Printf("User %v %v booked %v tickets using this email ID:%v\n",firstName,lastName,userTickets,email)
-}  
+	remaningTickets = remaningTickets - userTickets
+	// booking[0]=firstName +" " + lastName
+	booking = append(booking, firstName+" "+lastName)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. you will recevie a confirmation email at %v\n", firstName, lastName, userTickets, email)
+
+	fmt.Printf("%v tickets remaining for %v\n", remaningTickets, conferenceName)
+
+	fmt.Printf("These are all our bookings %v\n", booking)
+}
